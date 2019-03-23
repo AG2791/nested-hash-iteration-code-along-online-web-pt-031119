bcb049644986d2f1267fd2406a83149a8e2e36b0
AJ Guiles <ashiguiles@gmail.com>
2019-03-23 20:14:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |key, value| 
       if key == :favorite_ice_cream_flavors
-        favorite_ice_cream_flavors{
+    
         
         
 
